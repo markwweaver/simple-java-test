35508e2726230d56406ad8e50c0947f1ddbdd72b
Mark Weaver <mark@Oxygen.local>
2010-10-07 02:30:30 -0700

    Add a function.

--- Simple.java
+++ Simple.java
@@ -3,6 +3,11 @@
 public class Simple {
    public static void main(String[] args) {
       System.out.println("Hello, world!");
+      System.out.println(doubleNum(5));
+   }
+
+   public static int doubleNum(int num) {
+      return num * 2;
    }
 }
 
